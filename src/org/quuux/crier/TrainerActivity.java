da5c832b47fdf0369fd6f89478f65978e873fc5f
Marc DellaVolpe <marc@moe.(none)>
2009-04-03 09:09:53 -0400

    initial work on trainer

---
+++ src/org/quuux/crier/TrainerActivity.java
@@ -0,0 +1,20 @@
+package org.quuux.crier;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import org.quuux.crier.R;
+
+public class TrainerActivity extends Activity
+{
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+	
+    }
+
+}
