cf80401d932fbe8bb023b0857a478f104f247f25
Marc DellaVolpe <marc@moe.(none)>
2009-04-04 11:55:16 -0400

    added icon

--- src/org/quuux/crier/TrainerActivity.java
+++ src/org/quuux/crier/TrainerActivity.java
@@ -20,7 +20,9 @@ public class TrainerActivity extends Activity
 {
     private static final String TAG = "Crier";
 
-    static final int RECORD_PHRASE = 1;
+    static final int RESULT_RECORD_PHRASE = 1;
+
+    static final int DIALOG_PHRASE_TEXT = 1;
 
     public void onCreate(Bundle savedInstanceState)
     {
@@ -31,8 +33,8 @@ public class TrainerActivity extends Activity
 	
 	final Button button_add_phrase = (Button)findViewById(R.id.trainer_button_add_phrase);
 	button_add_phrase.setOnClickListener(new Button.OnClickListener() {
-		publicx void onClick(View v) {
-		    startActivityForResult(new Intent(MediaStore.Audio.Media.RECORD_SOUND_ACTION), RECORD_PHRASE);
+		public void onClick(View v) {
+		    startActivityForResult(new Intent(MediaStore.Audio.Media.RECORD_SOUND_ACTION), RESULT_RECORD_PHRASE);
 		}
 	    });
     }
@@ -41,9 +43,9 @@ public class TrainerActivity extends Activity
 	if(Config.LOGD)
 	    Log.d(TAG, "Result Intent: " + data);		
 
-         if(request_code == RECORD_PHRASE) {
+         if(request_code == RESULT_RECORD_PHRASE) {
              if(result_code == RESULT_OK) {	 
-		 
+		 showDialog(DIALOG_PHRASE_TEXT);
              }
          }
      }    
